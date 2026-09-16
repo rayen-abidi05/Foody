f499aa857133efe968caab5117a54ba663e3c50a
Abidi-Rayen <abidirayen7805@gmail.com>
2026-09-16 13:52:10 +0100

    fixing deploy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,9 +11,9 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true
-    }));
+}));
 
 app.use(cookieParser());
 
--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,10 +1,20 @@
-const mysql = require("mysql2")
-require('dotenv').config()
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 4000),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-})
+
+    ssl: {
+        minVersion: "TLSv1.2",
+    },
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+
 module.exports = pool.promise();
